76c6eb27e6458c35e2f0ebe02298226b7ee4fc7f
Amr Elhabbal <amrelhabbal1@gmail.com>
2019-05-19 07:19:15 -0500

    implement logger middleware

--- redux/src/index.js
+++ redux/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';//helper fun to inject the store into react component
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
@@ -16,10 +16,19 @@ const rootReducer = combineReducers({
    results: resultReducer
 });
 
+//implement logger middleware
+const logger = store => next => action => {
+   console.log('Middleware Dispatching', action);
+   const result = next(action);
+   console.log('Middleware next state', store.getState());
+   return result;
+}
+
 const store = createStore(
    rootReducer,
-   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+   applyMiddleware(logger),
 );
+
 
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
