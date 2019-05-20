8511dcf12f29cf68be9343aeefe41b5d2080713f
Amr Elhabbal <amrelhabbal1@gmail.com>
2019-05-19 23:21:57 -0500

    refactor result reducer

--- redux/src/store/reducers/result.js
+++ redux/src/store/reducers/result.js
@@ -6,18 +6,20 @@ const initialState = {
    results: []
 }
 
+const deleteResult = (state, actions) => {
+   let updatedResult = state.results.filter(result => {
+      return result.id !== actions.id;
+   });
+   return updateObject(state, {results: updatedResult}
+   );
+}
+
 const resultReducer = (state = initialState, actions) => {
    switch(actions.type) {
       case actionTypes.STORE_RESULT:
-            return updateObject(state, {results: state.results.concat({id: new Date(), value: actions.result})});
+         return updateObject(state, {results: state.results.concat({id: new Date(), value: actions.result})});
       case actionTypes.DELETE_RESULT:
-         let updatedResult = state.results.filter(result => {
-            return result.id !== actions.id;
-         })
-         return updateObject(
-            state,
-            {results: updatedResult}
-         );
+         return deleteResult(state, actions);
       default:
          return state;
    }
